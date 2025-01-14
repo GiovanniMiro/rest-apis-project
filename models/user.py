2a9efa742684a4e27e749495ce244f9949d5a327
Giovanni Miro <giovannimiropl@hotmail.com>
2025-01-14 09:29:10 -0300

    Add model changes and migrations, make sure migrations run before app start

--- models/user.py
+++ models/user.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from db import db
 
 class UserModel(db.Model):
@@ -6,4 +8,5 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
